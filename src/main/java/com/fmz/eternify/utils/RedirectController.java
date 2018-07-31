bcbc42e375e617e1669b5b379b2d7211e2483d9b
Faruk Zahra <farukz@gmail.com>
2018-07-31 13:35:23 -0300

    login

---
+++ src/main/java/com/fmz/eternify/utils/RedirectController.java
@@ -0,0 +1,18 @@
+package com.fmz.eternify.utils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RedirectController {
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String baseUrlRedirect(HttpServletRequest request, HttpServletResponse httpServletResponse) {
+		return "redirect:" + request.getRequestURL().append("login.xhtml").toString();
+	}
+
+}
