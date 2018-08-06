ba80465f13f71217d5a8ab05dcd9ea7664534db4
Faruk Zahra <farukz@gmail.com>
2018-08-06 08:54:58 -0300

    rest

--- src/main/java/com/fmz/eternify/controller/PessoaController.java
+++ src/main/java/com/fmz/eternify/controller/PessoaController.java
@@ -3,6 +3,7 @@ package com.fmz.eternify.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,14 @@ public class PessoaController {
 	@Autowired
 	PessoaRepository pessoaRepository;
 
-	@PostMapping("/addpessoa")
+	@PostMapping("/add")
 	public void addPessoa(@Valid @RequestBody Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
+	}
+	
+	@GetMapping("/get")
+	public Pessoa findPessoa(long id) {
+		return pessoaRepository.findById(id).get();
 	}
 
 }
